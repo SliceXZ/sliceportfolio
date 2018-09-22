d3f21e3d11109b5b88fd26705343206866d6d30b
SliceXZ <az_95@hotmail.com>
2018-09-22 16:40:13 -0700

    changed link to home page to be explicitly index.html, changed index cover photo from jpg to png, cover photo url is a live one instead of a local one, added styling to contact page

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ var sass = require('gulp-sass');
 var browserSync = require('browser-sync').create();
 
 gulp.task('sass', function() {
-    return gulp.src('./scss/materialize.scss')
+    return gulp.src('./scss/*.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest('./css'))
         .pipe(browserSync.stream());
